75477fa8ac9f936bcf0936c9af59e3e712debb8c
Paavani1 <paavani549@gmail.com>
2023-03-06 23:05:07 +0530

    webdriver commit

---
+++ test/specs/laraclassified/addproducttofav.js
@@ -0,0 +1,7 @@
+describe('addfavproduct',async()=>{
+    it('logintoapplication',async()=>{
+        await browser.url("http://122.166.192.191:9003/")
+        await browser.getTitle()
+    })
+
+})
---
+++ test/specs/laraclassified/practice1.js
@@ -0,0 +1,3 @@
+const expectchai=require("chai")
+var text = 'js';
+await assert.isString(text, 'it is a string');
